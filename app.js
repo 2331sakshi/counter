0c742baf432c44b82f59c872a2de52904a25bbe6
skshp <skshpanchal@gmail.com>
2022-06-13 16:24:33 +0530

    initial

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 //set initial count 
-let count =0;
+let count = 0;
 const btns = document.querySelectorAll('.btn');
 //console.log(btns);
 const myValue = document.getElementById('value');
 
 //EVENT LISTENERS
-btns.forEach(function(btn){
+btns.forEach(function (btn) {
     //console.log(btn)
-    btn.addEventListener('click',function(e){ //e is an event object which has properties like current target
+    btn.addEventListener('click', function (e) { //e is an event object which has properties like current target
         const styles = e.currentTarget.classList;
-        if(styles.contains('decrease')){
+        if (styles.contains('decrease')) {
             count--;
-        } else if(styles.contains('reset')){
-            count=0;
-        } else{
-            
+        } else if (styles.contains('reset')) {
+            count = 0;
+        } else {
+
             count++;
+
         }
-        if(count>0){
+        if (count > 0) {
             myValue.style.color = 'green';
-        } else if(count<0){
+        } else if (count < 0) {
             myValue.style.color = 'red';
         } else myValue.style.color = '#222';
-        myValue.textContent=count;
+        myValue.textContent = count;
     });
 });
