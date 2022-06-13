c8bf03ba7a85632e8173b9a8f2978d23dab6e38f
skshp <skshpanchal@gmail.com>
2022-06-13 15:55:43 +0530

    Initial

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ btns.forEach(function(btn){
         } else if(styles.contains('reset')){
             count=0;
         } else{
+            
             count++;
         }
         if(count>0){
