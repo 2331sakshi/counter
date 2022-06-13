8e4bdcc7c45ffdf3f046c1fb803299e9a4dd1d32
skshp <skshpanchal@gmail.com>
2022-06-13 15:49:58 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+//set initial count 
+let count =0;
+const btns = document.querySelectorAll('.btn');
+//console.log(btns);
+const myValue = document.getElementById('value');
+
+//EVENT LISTENERS
+btns.forEach(function(btn){
+    //console.log(btn)
+    btn.addEventListener('click',function(e){ //e is an event object which has properties like current target
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')){
+            count--;
+        } else if(styles.contains('reset')){
+            count=0;
+        } else{
+            count++;
+        }
+        if(count>0){
+            myValue.style.color = 'green';
+        } else if(count<0){
+            myValue.style.color = 'red';
+        } else myValue.style.color = '#222';
+        myValue.textContent=count;
+    });
+});
